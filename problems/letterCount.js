f2affc5cebc710d7f9ff5ee8b8a3c215e32024d5
Coreen Cooper <coreencooper@pursuit.org>
2021-08-12 23:40:58 -0400

    update files

--- problems/letterCount.js
+++ problems/letterCount.js
@@ -8,41 +8,33 @@
  */
 
  // try with forEach 
-function letterCount(str) { 
-    let newArr = str.toLowerCase().split(" ");
-    let strNoSpace = newArr.join("");
-    let obj = {};
-    for (let i = 0; i < strNoSpace.length; i++) {
-      const el = strNoSpace[i];
-      obj[el] ? obj[el] += 1 : obj[el] = 1;
-    }
-    return obj;
-}
-
-// function letterCount(str) { // debug
-//   str = str.toLowerCase();
-//   let obj = {};
-//   for (const char of str) {
-//     if(char !== " ") {
-//         if(obj[char]){
-//           obj[char] + 1;
-//         } else {
-//           obj[char] = 1;
-//         }
-//     } 
-    // console.log(char)
-
-    // function letterCount(str) {
+// function letterCount(str) { 
 //     let newArr = str.toLowerCase().split(" ");
 //     let strNoSpace = newArr.join("");
 //     let obj = {};
-
-//   }
-//   return obj
+//     for (let i = 0; i < strNoSpace.length; i++) {
+//       const el = strNoSpace[i];
+//       if (obj[el]) {
+//         obj[el] += 1
+//       } else {
+//         obj[el] = 1};
+//     }
+//     return obj;
 // }
 
-
-// letterCount("letterCount")
-
-
+function letterCount(str) {
+  str = str.toLowerCase();
+  let obj = {};
+  for (const char of str) { // for each charcter in a string, we're going to do something {}
+    if(char !== " ") {
+        if(obj[char]) {
+          obj[char] += 1;
+        } else {
+          obj[char] = 1;
+        }
+    }
+  }
+  return obj
+} 
+  
 module.exports = letterCount;
